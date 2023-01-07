10da64102ff523aaed7f9d217c7a969d6808f744
Max Tootleman <piddle99@gmail.com>
2023-01-07 17:49:52 -0500

    finally

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://github.com',
-  baseUrl: '/piddle99.github.io/',
+  url: 'https://piddle99.github.io',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
