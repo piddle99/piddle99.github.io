b720b64aa76d583cc23bd863302f76f25b80854f
Max Tootleman <piddle99@gmail.com>
2023-01-07 20:42:01 -0500

    finally

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,20 +34,6 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -70,7 +56,31 @@ const config = {
             position: 'left',
             label: 'Home',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            to: '/resume',
+            label: 'Resume',
+            position: 'left'
+          },
+          {
+            to: '/acting',
+            label: 'Acting',
+            position: 'left'
+          },
+          {
+            to: '/directing',
+            label: 'Directing',
+            position: 'left'
+          },
+          {
+            to: '/gallery',
+            label: 'Gallery',
+            position: 'left'
+          },
+          {
+            to: '/contact',
+            label: 'Contact',
+            position: 'left'
+          },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
